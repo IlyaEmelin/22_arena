87bee31e44d2fa7f790c89ba3d65a36890204ecc
Илья <bkmz8_133@mail.ru>
2025-06-15 10:01:44 +0300

    hello

--- thing.py
+++ thing.py
@@ -1,9 +1,31 @@
+from random import choice, uniform
+
 from thing_type import ThingType
+
 
 class Thing:
     """Предмет"""
-    name: str
-    percent_protection: float  # процент защиты
-    attack: float  # атаку
-    life: float  # жизнь
-    thing_type: ThingType
+
+    def __init__(self):
+        self.name: str = None
+        self.percent_protection: float = None  # процент защиты
+        self.attack: float = None  # атаку
+        self.life: float = None  # жизнь
+        self.thing_type: ThingType = None  # тип предмета
+
+    def __str__(self):
+        return (
+            f"{self.name} c защитой {self.percent_protection} "
+            f"и атакой {self.attack} "
+            f"и защитой {self.percent_protection}"
+            f"и жизнью {self.life}"
+        )
+
+
+def generate_item(thing_type: ThingType) -> "Thing":
+    new_thing = Thing()
+    new_thing.name = choice(thing_type[1])
+    new_thing.attack = uniform(1, 10)
+    new_thing.life = uniform(10, 100)
+    new_thing.thing_type = thing_type
+    return new_thing
