291517fc0ee0a5b1b81bbfe5dd9f156fdcd93c3d
Aleksandr Zhiliaev <darkbasilur@gmail.com>
2025-06-15 20:33:27 +1200

    print

--- main.py
+++ main.py
@@ -4,21 +4,8 @@ from thing_type import ThingType
 from thing import Thing, generate_item
 from Person import generate_person
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def main():
-    # # Use a breakpoint in the code line below to debug your script.
-    # thing_type = choice(ThingType.all)
-    # print("thing_type:", thing_type)
-    # thing = generate_item(thing_type)
-    # print("thing:", thing)
-    #
-    # person = generate_person()
-    # print("person:", person)
 
     persons = []
     print("Создание персонажей:")
@@ -37,7 +24,8 @@ def main():
         if not defender.take_damage(attack):
             persons.pop(defender_index)
 
-        print(f"Пользователь {attaker} нанес урон {defender}, в размере {attack}")
+        print(
+            f"Пользователь {attaker} нанес урон {defender}, в размере {defender.damage:.0f}")
 
     print("Победитель:", persons[0])
 
@@ -50,8 +38,5 @@ def get_attaker(defender, persons):
     return attaker
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
