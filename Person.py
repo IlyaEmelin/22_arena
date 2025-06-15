0cb57b99cc7b71c0b2d3038d7f75f770b0e673e2
Илья <bkmz8_133@mail.ru>
2025-06-15 10:28:53 +0300

    hello

--- Person.py
+++ Person.py
@@ -1,12 +1,18 @@
+from random import choice, uniform
+
+from thing_type import ThingType
+from thing import Thing, generate_item
+
+
 class Person:
     """Класс персонажа"""
 
     def __init__(
-            self,
-            name: str,
-            health: int,
-            base_attack: int,
-            base_defense: float,
+        self,
+        name: str,
+        health: int,
+        base_attack: int,
+        base_defense: float,
     ) -> None:
         """Базовый класс персонажа."""
         self.name = name
@@ -30,12 +36,14 @@ class Person:
             total_defense += thing.percent_protection
 
         if total_defense > 1:
-            raise Exception('total_defense > 1')
+            raise Exception("total_defense > 1")
         damage = attack * (1 - total_defense)
         self.health -= int(damage)
 
-        print(f'''{self.name} получил {int(damage)} урона.
-            Осталось HP: {self.health}''')
+        print(
+            f"""{self.name} получил {int(damage)} урона.
+            Осталось HP: {self.health}"""
+        )
 
         return self.health > 0
 
@@ -49,3 +57,41 @@ class Person:
     def __str__(self) -> str:
         """Строковое представление персонажа"""
         return f"{self.name}: HP={self.health}"
+
+
+names = (
+    "Алариэль",
+    "Селестинара",
+    "Элариондар",
+    "Лираэльна",
+    "Брандарсон",
+    "Ариаднара",
+    "Кэл’ариэль",
+    "Эльрианора",
+)
+
+
+def generate_person():
+    person = Person(
+        choice(names),
+        uniform(100, 1000),
+        uniform(0.01, 0.1),
+        uniform(10, 50),
+    )
+
+    added_thing = list()
+    added_thing_type = set()
+    count = 0
+    while True:
+        thing_type = choice(ThingType.all)
+        if thing_type in added_thing_type:
+            continue
+        added_thing_type.add(thing_type)
+
+        thing = generate_item(thing_type)
+        added_thing.append(thing)
+        count += 1
+        if count > 4:
+            break
+    person.set_things(added_thing)
+    return person
